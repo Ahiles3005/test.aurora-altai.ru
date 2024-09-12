8583375869a44603db885fe97a5a90f814eb8289
Одногулов Руслан Хосянович <orh@cds.guru>
2024-09-12 14:38:25 +0300

    fix

--- www/test.aurora-altai.ru/bitrix/templates/.default/components/bitrix/news/how-to/bitrix/news.detail/.default/result_modifier.php
+++ www/test.aurora-altai.ru/bitrix/templates/.default/components/bitrix/news/how-to/bitrix/news.detail/.default/result_modifier.php
@@ -10,7 +10,7 @@ if($arResult['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'] && !$arResult['DISPLA
 	$arResult['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'][] = $tmp;
 }
 
-for($i=0;$i<count($arResult['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE']);$i++){
+for($i=0;$i<count($arResult['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'] ?? []);$i++){
 	$FILE_ID = $arResult['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'][$i]['ID'];
 	$arResult['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'][$i] = CFile::ResizeImageGet($FILE_ID, array('width'=>1280,'height'=>900), BX_RESIZE_IMAGE_PROPORTIONAL);
 	$arResult['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'][$i]["PREVIEW"] = CFile::ResizeImageGet($FILE_ID, array('width'=>730,'height'=>487), BX_RESIZE_IMAGE_EXACT);
