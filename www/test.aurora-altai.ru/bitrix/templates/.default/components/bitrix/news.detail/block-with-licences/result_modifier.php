aeefdad42410e803d8d857c413f978968c2248f8
Одногулов Руслан Хосянович <orh@cds.guru>
2024-09-12 14:49:42 +0300

    fix

--- www/test.aurora-altai.ru/bitrix/templates/.default/components/bitrix/news.detail/block-with-licences/result_modifier.php
+++ www/test.aurora-altai.ru/bitrix/templates/.default/components/bitrix/news.detail/block-with-licences/result_modifier.php
@@ -2,4 +2,4 @@
 for($j=0;$j<count($arResult['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE']);$j++){
 	$arResult['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'][$j]['PREVIEW'] = CFile::ResizeImageGet($arResult['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'][$j]['ID'], array('width'=>350,'height'=>504), BX_RESIZE_IMAGE_EXACT/*BX_RESIZE_IMAGE_PROPORTIONAL*/);
 }
-/*<pre><?print_r($arResult['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'])?></pre>
+
