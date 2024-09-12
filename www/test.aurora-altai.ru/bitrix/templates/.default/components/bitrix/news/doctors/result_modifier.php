464792681821beca91cda36bb33b8a49b8dda86d
Одногулов Руслан Хосянович <orh@cds.guru>
2024-09-12 14:47:17 +0300

    fix

--- www/test.aurora-altai.ru/bitrix/templates/.default/components/bitrix/news/doctors/result_modifier.php
+++ www/test.aurora-altai.ru/bitrix/templates/.default/components/bitrix/news/doctors/result_modifier.php
@@ -1,15 +1,15 @@
 <?
-for($i=0;$i<count($arResult["ITEMS"]);$i++){
-	if(!$arResult["ITEMS"][$i]['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'][0]['SRC']){
-		$tmp = $arResult["ITEMS"][$i]['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'];
-		$arResult["ITEMS"][$i]['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'] = array();
-		$arResult["ITEMS"][$i]['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'][] = $tmp;
-	}
+for ($i = 0; $i < count($arResult["ITEMS"] ?? []); $i++) {
+    if (!$arResult["ITEMS"][$i]['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'][0]['SRC']) {
+        $tmp = $arResult["ITEMS"][$i]['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'];
+        $arResult["ITEMS"][$i]['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'] = array();
+        $arResult["ITEMS"][$i]['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'][] = $tmp;
+    }
 }
 
-for($i=0;$i<count($arResult["ITEMS"]);$i++){
-	for($j=0;$j<count($arResult["ITEMS"][$i]['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE']);$j++){
-		$arResult["ITEMS"][$i]['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'][$j] = CFile::ResizeImageGet($arResult["ITEMS"][$i]['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'][$j]['ID'], array('width'=>640,'height'=>480), BX_RESIZE_IMAGE_PROPORTIONAL);
-	}
+for ($i = 0; $i < count($arResult["ITEMS"] ?? []); $i++) {
+    for ($j = 0; $j < count($arResult["ITEMS"][$i]['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'] ?? []); $j++) {
+        $arResult["ITEMS"][$i]['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'][$j] = CFile::ResizeImageGet($arResult["ITEMS"][$i]['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'][$j]['ID'], array('width' => 640, 'height' => 480), BX_RESIZE_IMAGE_PROPORTIONAL);
+    }
 }
 
